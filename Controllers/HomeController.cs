32b64f1a0a0d12b12311ad28c5ce00f2238e1b95
Raheem Abdulhaqq <raheemabdulhaqq2020@gmail.com>
2021-07-04 17:35:04 +0100

    implemented cv download

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,5 +29,39 @@ namespace MichealOgundero.Controllers
             }
 
         }
+
+        public FileResult DownloadResumeDocx()
+        {
+            //Name the File
+            string fileName = "ogundero_micheal_ayodeji.docx";
+
+            //Build the File Path.
+            string path = Server.MapPath("~/documents/ogundero_micheal_ayodeji.docx");
+
+            //Read the File data into Byte Array.
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+
+            //Send the File to Download.
+            return File(bytes, "application/octet-stream", fileName);
+
+
+
+
+        }
+        public FileResult DownloadResumePdf()
+        {
+            //Name the File
+            string fileName = "ogundero_micheal_ayodeji.pdf";
+
+            //Build the File Path.
+            string path = Server.MapPath("~/documents/ogundero_micheal_ayodeji_CV.pdf");
+
+            //Read the File data into Byte Array.
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+
+            //Send the File to Download.
+            return File(bytes, "application/octet-stream", fileName);
+
+        }
     }
 }
